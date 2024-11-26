cf17f3df8c6888d1bde3c184af7d8e7133030df9
Arturo <artsim3088@gmail.com>
2024-11-26 14:41:09 -0500

    hello.go, split off the language selection portion into another function, greetingPrefix. Added constants for each language choice and the language greeting.
    hello_test.go, added tests for spanish, french, and russian language greetings.

--- hello/hello.go
+++ hello/hello.go
@@ -3,14 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
 
 const englishHelloPrefix = "Hello, "
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hola, "
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
+const russian = "Russian"
+const russianHelloPrefix = "Privet, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case russian:
+		prefix = russianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }

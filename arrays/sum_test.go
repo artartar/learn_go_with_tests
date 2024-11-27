14529640c38b01e8e2d3f6a47803ce76e7425be6
artartar <artsim3088@gmail.com>
2024-11-26 22:56:20 -0500

    Added SumAll function - takes any number of slices as input, sums each slice using the Sum function and appends the results to a slice. Added test for SumAll function

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	// "reflect"
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
@@ -13,4 +17,21 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	// Introduced in or after Go 1.21 and is part of the slices standard library. Does a simple and shallow comparison of slices.
+	// First checks if they are the same size. Different sizes equals failure
+	// If sizes are the same, compares each value in the arrays 1 by 1 until it encounters a failure. No failure equals success(equal)
+	if !slices.Equal[[]int](got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+
+	// Not type safe, implemented as directed in the course.
+	// if !reflect.DeepEqual(got, want) {
+	// 	t.Errorf("got %v want %v", got, want)
+	// }
 }
